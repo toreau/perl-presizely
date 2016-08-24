1a0e74967ccce793c8b10a6627322474f841de6c
Tore Aursand <toreau@gmail.com>
2016-08-24 12:47:35 +0200

    Minor fix.

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-# ./script/presizely prefork -m production -w 32 -c 8 -P /tmp/presizely.pid -l http://*:3000?reuse=1
-./script/presizely prefork -m production -w 16 -c 2 &
--- run_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-MOJO_MAX_MESSAGE_SIZE=33554432 ./script/presizely prefork -m development -w 8 -c 2 -P /tmp/presizely.pid -l http://*:3000?reuse=1
